4bb568ab719a80c2ba355b124333163f536d2f02
Kailey Liou <kaileyliou09@gmail.com>
2026-08-08 15:04:31 -0400

    completed practice making shapes

--- interactive_avatar/interactive_avatar_part_2_challenge/script.js
+++ interactive_avatar/interactive_avatar_part_2_challenge/script.js
@@ -6,27 +6,32 @@ function setup() {
   createCanvas(400, 400); 
 
   //sets the background color
-  background(255,249,226);  
+  background(255, 249, 226);  
 }
 
 /* DRAW LOOP REPEATS */
 function draw() {
+  angleMode(DEGREES);
+  rectMode(CENTER);
   
   // Draw an ellipse
-
+  ellipse(60, 320, 100, 150);
 
   // Draw a line
-
+  line(150, 380, 380, 380);
   
   // Draw a rectangle 
-
+  rect(200, 200, 120, 40);
   
   // Draw an arc
-
+  arc(320, 50, 100, 100, 0, 180);
   
   // Sign your name
-
+  textSize(40);
+  text('Kailey', 150, 380);
   
   // Add a short description
+  textSize(15);
+  text('This is a piece of art \nwe created to practice \ndrawing shapes and text \nto a p5.js sketch.', 20, 20);
 
 }

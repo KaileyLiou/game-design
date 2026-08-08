96aaf2c898f51b71231f58917297300b286be32a
Kailey Liou <kaileyliou09@gmail.com>
2026-08-08 14:50:08 -0400

    uploaded practice files

---
+++ interactive_avatar/interactive_avatar_part_2_challenge/script.js
@@ -0,0 +1,32 @@
+/* VARIABLES */
+
+/* SETUP RUNS ONCE */
+function setup() {
+  //sets the screen size
+  createCanvas(400, 400); 
+
+  //sets the background color
+  background(255,249,226);  
+}
+
+/* DRAW LOOP REPEATS */
+function draw() {
+  
+  // Draw an ellipse
+
+
+  // Draw a line
+
+  
+  // Draw a rectangle 
+
+  
+  // Draw an arc
+
+  
+  // Sign your name
+
+  
+  // Add a short description
+
+}

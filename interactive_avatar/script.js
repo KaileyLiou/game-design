845212c4b4dc56f556ce2af03ce7e895f5aa4e75
Kailey Liou <kaileyliou09@gmail.com>
2026-08-08 15:15:59 -0400

    created face

--- interactive_avatar/script.js
+++ interactive_avatar/script.js
@@ -11,5 +11,25 @@ function setup() {
 
 /* DRAW LOOP REPEATS */
 function draw() {
+  angleMode(DEGREES);
+  rectMode(CENTER);
+
+  //Face
+  ellipse(200, 200, 175, 200);
+
+  //Eyes
+  ellipse(170, 170, 50, 40);
+  ellipse(230, 170, 50, 40);
+
+  //Pupils
+  ellipse(170, 170, 22, 25);
+  ellipse(230, 170, 22, 25);
+
+  //Mouth
+  arc(200, 230, 50, 50, 0, 180);
+
+  //Text
+  textSize(15);
+  text('We will customize this \ntext later on in the \nproject to reflect your avatar.', 20, 20);
   
 }

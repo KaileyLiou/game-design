e996ca0a5e6baea5fa8d3fbd1b3fa8c22bf8f01b
Kailey Liou <kaileyliou09@gmail.com>
2026-08-09 22:43:30 -0400

    added catcher movement

--- collection_game/script.js
+++ collection_game/script.js
@@ -11,7 +11,6 @@ function preload(){
 /* SETUP RUNS ONCE */
 function setup() {
   createCanvas(400,400);
-  background(224,224,224);
   
   //Create catcher 
   catcher = new Sprite(200,380,100,20);
@@ -20,16 +19,39 @@ function setup() {
   //Create falling object
   fallingObject = new Sprite(100,0,10);
   fallingObject.color = color(0,128,128);
-
+  fallingObject.vel.y = 2;
 }
 
 /* DRAW LOOP REPEATS */
 function draw() {
+  background(224,224,224);
   
   // Draw directions to screen
   fill(0);
   textSize(12);
   text("Move the \ncatcher with the \nleft and right \narrow keys to \ncatch the falling \nobjects.", width-100, 20);
 
-  
+  // If fallingObject reaches bottom, move back to random position at top
+  if(fallingObject.y >= height){
+    fallingObject.y = 0;
+    fallingObject.x = random(width);
+    fallingObject.vel.y = random(1, 5);
+  }
+
+  // Move catcher
+  if(kb.pressing("left")) {
+    catcher.vel.x = -3;
+  } else if(kb.pressing("right")) {
+    catcher.vel.x = 3;
+  } else {
+    catcher.vel.x = 0;
+  }
+
+  // Stop catcher at edges of screen
+  if(catcher.x < 50) {
+    catcher.x = 50;
+  } else if(catcher.x > 350) {
+    catcher.x = 350;
+  }
+
 }
